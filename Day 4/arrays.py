4b744715c1d6e64b7b41853dc88bc08881441104
R-Owino <sayeakinyi@gmail.com>
2024-03-18 00:32:40 +0300

    update: add exercises 2 and 3

--- Day 4/arrays.py
+++ Day 4/arrays.py
@@ -51,9 +51,54 @@ def expense_tracker():
     print(f"Expense after adding June: {expense}")
 
     # Ali returned an item that he bought in April and a got a refund of 200.
-    expense[3] += 200
+    expense[3] -= 200
     print(f"Expense after refund: {expense}")
 
 expense_tracker()
 
 print("###################### Example 2 ######################")
+"""
+Given this list of marvel characters, find out:
+    characters = ['spider man','thor','hulk','iron man','captain america']
+- The length of the list
+- Add "black panther" at the end of this list
+- Remove "black panther" from the list and add it after "hulk"
+- Using one line of code, remove "hulk" and "thor" and replace them with "doctor strange"
+- Sort the list in aplhabetical order
+"""
+
+def marvel():
+    characters = ['spider man','thor','hulk','iron man','captain america']
+    print(f"Length of list: {len(characters)}")
+
+    # Add "black panther" at the end of this list
+    characters.append("black panther")
+    print(f"List after adding 'black panther': {characters}")
+
+    # Remove "black panther" from the list and add it after "hulk"
+    characters.remove("black panther")
+    characters.insert(3, "black panther")
+    print(f"List after removing 'black panther' and adding it after 'hulk': {characters}")
+
+    # Using one line of code, remove "hulk" and "thor" and replace them with "doctor strange"
+    characters[1:3] = ["doctor strange"]
+    print(f"List after removing 'hulk' and 'thor' and adding 'doctor strange': {characters}")
+
+    # Sort the list in aplhabetical order
+    characters.sort()
+    print(f"List after sorting in aplhabetical order: {characters}")
+
+marvel()
+
+print("###################### Example 3 ######################")
+"""
+Create a list of odd numbers between 1 and a max number
+Take max_number as input from the user
+"""
+
+def odd_numbers():
+    max_number = int(input("Enter max number: "))
+    odd = [x for x in range(1, max_number + 1) if x % 2 == 1]
+    print(f"Odd numbers between 1 and {max_number}: {odd}")
+
+odd_numbers()
