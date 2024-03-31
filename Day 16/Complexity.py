bfd306101eaaadd0be03f85732b786bf691e70b9
R-Owino <sayeakinyi@gmail.com>
2024-04-01 00:07:05 +0300

    update: add demos for exponential and logarithmic complexities

--- Day 16/Complexity.py
+++ Day 16/Complexity.py
@@ -29,4 +29,52 @@ index = 0
 print(find_first_pe(prices, eps, index))
 
 # Example 3 -  quadratic complexity
+def find_duplicates(nums):
+    """
+    finds duplacates in a list of numbers
+    """
+    duplicates = []
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] == nums[j]:
+                duplicates.append(nums[i])
+    return duplicates
 
+numbers = [3, 4, 6, 4, 7, 9, 3]
+res = find_duplicates(numbers)
+print(f"{res} is a duplicate in the list")
+
+# Example 4 -  exponential complexity
+def fib(n):
+    """
+    Returns a list containing the Fibonacci sequence up to n
+    """
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+n = 10
+print(fib(n))
+
+# Example 5 -  logarithmic complexity
+def binary_search(nums, target):
+    """
+    Binary search algorithm
+    """
+    left, right = 0, len(nums)-1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 8
+print(binary_search(nums, target))
